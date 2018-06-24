f4514d13957e2fd224f7fd713cef9dc4bcf2819c
Pierre Francelino <pierre.francelino@sap.com>
2018-06-24 17:57:05 -0300

    Refatorção principal

--- src/main/java/br/edu/ulbra/artigocientifico/repository/TipoVinhoRepository.java
+++
@@ -1,8 +0,0 @@
-package br.edu.ulbra.artigocientifico.repository;
-
-import br.edu.ulbra.artigocientifico.model.TipoVinho;
-import org.springframework.data.repository.CrudRepository;
-
-public interface TipoVinhoRepository extends CrudRepository<TipoVinho, Long> {
-
-}
--- src/main/java/br/edu/ulbra/artigocientifico/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package br.edu.ulbra.artigocientifico.repository;
-
-import br.edu.ulbra.artigocientifico.model.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Long> {
-
-	User findByUsername(String username);
-}
