e7897da3d23e79f6ce2b73d811cae3eefb25e994
Pierre Francelino <pierre.francelino@sap.com>
2018-06-26 21:48:08 -0300

    Refactor in controllers

--- src/main/java/br/edu/ulbra/artigoCientifico/controller/IndexController.java
+++ src/main/java/br/edu/ulbra/artigoCientifico/controller/IndexController.java
@@ -1,22 +1,16 @@
 package br.edu.ulbra.artigoCientifico.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.core.io.FileSystemResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.edu.ulbra.artigoCientifico.config.StringConstants;
 import br.edu.ulbra.artigoCientifico.model.Evento;
-import br.edu.ulbra.artigoCientifico.model.Role;
 import br.edu.ulbra.artigoCientifico.repository.EventoRepository;
 import br.edu.ulbra.artigoCientifico.service.interfaces.SecurityService;
 
-//import java.nio.file.Paths;
 import java.util.List;
 
 @Controller
@@ -37,9 +31,9 @@ public class IndexController {
 		mv.addObject("userLogged", securityService.findLoggedInUser());
 		mv.addObject(StringConstants.ADMIN, true);
 		List<Evento> eventos = (List<Evento>) eventoRepository.findAll();
-		mv.addObject("wines", eventos);
+		mv.addObject("events", eventos);
 		return mv;
-	}
+	} 
 
 	@RequestMapping("/minhalista")
 	public ModelAndView minhalista() {
@@ -48,11 +42,6 @@ public class IndexController {
 		mv.addObject(StringConstants.ADMIN, true);
 		return mv;
 	}
-
-//	@ResponseBody
-//	public FileSystemResource getFile(@PathVariable("fileName") String fileName){
-//		return new FileSystemResource(Paths.get(uploadFilePath, fileName).toString());
-//	}
 
 	@GetMapping("/login")
 	public ModelAndView loginForm(){
--- src/main/java/br/edu/ulbra/artigoCientifico/controller/UserController.java
+++ src/main/java/br/edu/ulbra/artigoCientifico/controller/UserController.java
@@ -3,13 +3,11 @@ package br.edu.ulbra.artigoCientifico.controller;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import br.edu.ulbra.artigoCientifico.config.RedirectConstants;
 import br.edu.ulbra.artigoCientifico.config.StringConstants;
 import br.edu.ulbra.artigoCientifico.input.UserInput;
 import br.edu.ulbra.artigoCientifico.model.Role;
@@ -20,72 +18,156 @@ import br.edu.ulbra.artigoCientifico.service.interfaces.SecurityService;
 import br.edu.ulbra.artigoCientifico.service.interfaces.UserService;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Controller
-@RequestMapping("/usuario")
+@RequestMapping("/usuarios")
 public class UserController {
+	@Autowired
+	SecurityService securityService;
 	@Autowired
 	UserRepository userRepository;
 	@Autowired
 	UserService userService;
 	@Autowired
 	RoleRepository roleRepository;
-	@Autowired
-	SecurityService securityService;
 
 	private ModelMapper mapper = new ModelMapper();
 
+	@RequestMapping()
+	public ModelAndView listaUsuarios() {
+		ModelAndView mv = new ModelAndView("usuario/lista");
+		mv.addObject(StringConstants.USER_LOGGED, securityService.findLoggedInUser());
+
+		mv.addObject(StringConstants.ADMIN, true);
+		List<User> usuarios = (List<User>) userRepository.findAll();
+		mv.addObject("users", usuarios);
+		return mv;
+	}
+
 	@GetMapping("/novo")
-	public ModelAndView newUserForm(@ModelAttribute("user") UserInput user){
-		ModelAndView mv = new ModelAndView("user/new");
+	public ModelAndView novoUsuarioForm(@ModelAttribute("user") UserInput user){
+		List<Role> roles = (List<Role>)roleRepository.findAll();
+		ModelAndView mv = new ModelAndView("usuario/novo");
+		mv.addObject(StringConstants.USER_LOGGED, securityService.findLoggedInUser());
+		mv.addObject(StringConstants.ADMIN, true);
+		mv.addObject("roles", roles);
 		mv.addObject("user", user);
-
-		if (securityService.findLoggedInUser() != null && securityService.findLoggedInUser().getRoles() != null) {
-			for(Role p : securityService.findLoggedInUser().getRoles()){
-				if (p.getName().equals("ROLE_ADMIN")) {
-					mv.addObject("admin", true);
-					break;
-				}
-				else {
-					mv.addObject("admin", false);
-				}
-			}
-		}
-
 		return mv;
 	}
 
 	@PostMapping("/novo")
-	public String newUser(UserInput userInput, RedirectAttributes redirectAttrs){
+	public String novoUsuario(UserInput userInput, RedirectAttributes redirectAttrs){
 		User usuario = userRepository.findByUsername(userInput.getUsername());
 
 		if (usuario != null) {
 			redirectAttrs.addFlashAttribute(StringConstants.ERROR, "Um usuário com esse email já está cadastrado.");
 			redirectAttrs.addFlashAttribute("user", userInput);
-			return "redirect:/usuario/novo";
+			return RedirectConstants.REDIRECT_USUARIO_NOVO;
 		}
 
 		if (userInput.getPassword().length() == 0) {
 			redirectAttrs.addFlashAttribute(StringConstants.ERROR, "Uma senha deve ser informada.");
 			redirectAttrs.addFlashAttribute("user", userInput);
-			return "redirect:/admin/usuario/novo";
+			return RedirectConstants.REDIRECT_USUARIO_NOVO;
 		}
 
 		if (!userInput.getPassword().equals(userInput.getPasswordConfirm())) {
 			redirectAttrs.addFlashAttribute(StringConstants.ERROR, "Senha e confirmação de senha não são iguais.");
 			redirectAttrs.addFlashAttribute("user", userInput);
-			return "redirect:/usuario/novo";
+			return RedirectConstants.REDIRECT_USUARIO_NOVO;
 		}
 
-		Role role = roleRepository.findByName("ROLE_USER");
+		Role role = roleRepository.findById(userInput.getIdRole()).get();
 		User user = mapper.map(userInput, User.class);
 		Set<Role> roles = new HashSet<>();
 		roles.add(role);
 		user.setRoles(roles);
 		userService.save(user);
 
-		redirectAttrs.addFlashAttribute("success", "Usuário cadastrado com sucesso. Você já pode entrar no sistema.");
-		return "redirect:/";
+		redirectAttrs.addFlashAttribute(StringConstants.SUCCESS, "Usuário cadastrado com sucesso.");
+		return RedirectConstants.REDIRECT_USUARIO;
+	}
+
+	@GetMapping("/{id}")
+	public ModelAndView detalheUsuario(@PathVariable("id") Long idUsuario, RedirectAttributes redirectAttrs){
+		User usuario = userRepository.findById(idUsuario).get();
+
+		if (usuario == null) {
+			redirectAttrs.addFlashAttribute(StringConstants.ERROR, "O usuário solicitado não existe.");
+			return new ModelAndView(RedirectConstants.REDIRECT_USUARIO);
+		}
+
+		UserInput userInput = mapper.map(usuario, UserInput.class);
+
+		Set<Role> userRoles = usuario.getRoles();
+		if (!userRoles.isEmpty()){
+			userInput.setIdRole(userRoles.iterator().next().getId());
+		}
+
+		List<Role> roles = (List<Role>)roleRepository.findAll();
+
+		ModelAndView mv = new ModelAndView("usuario/detalhe");
+		mv.addObject(StringConstants.USER_LOGGED, securityService.findLoggedInUser());
+
+		mv.addObject(StringConstants.ADMIN, true);
+		mv.addObject("roles", roles);
+		mv.addObject("user", userInput);
+		return mv;
+	}
+
+	@PostMapping("/{id}")
+	public String salvarUsuario(@PathVariable("id") Long idUsuario, UserInput userInput, RedirectAttributes redirectAttrs) {
+		User usuario = userRepository.findById(idUsuario).get();
+
+		if (usuario == null) {
+			redirectAttrs.addFlashAttribute(StringConstants.ERROR, "Esse usuário não existe.");
+			redirectAttrs.addFlashAttribute("user", userInput);
+			return "redirect:/usuarios/" + idUsuario;
+		}
+
+		User usuarioTest = userRepository.findByUsername(userInput.getUsername());
+		if (usuarioTest != null && !usuario.getUsername().equals(usuarioTest.getUsername())) {
+			redirectAttrs.addFlashAttribute(StringConstants.ERROR, "Um usuário com esse email já está cadastrado.");
+			redirectAttrs.addFlashAttribute("user", userInput);
+			return RedirectConstants.REDIRECT_USUARIO + idUsuario;
+		}
+
+		if (userInput.getPassword().length() != 0 && !userInput.getPassword().equals(userInput.getPasswordConfirm())) {
+			redirectAttrs.addFlashAttribute(StringConstants.ERROR, "Senha e confirmação de senha não são iguais.");
+			redirectAttrs.addFlashAttribute("user", userInput);
+			return RedirectConstants.REDIRECT_USUARIO + idUsuario;
+		}
+
+		usuario.setName(userInput.getName());
+		usuario.setUsername(userInput.getUsername());
+		if (userInput.getPassword().length() != 0) {
+			usuario.setPassword(userInput.getPassword());
+		}
+
+		userService.save(usuario);
+
+		redirectAttrs.addFlashAttribute(StringConstants.SUCCESS, "Usuário alterado com sucesso.");
+		redirectAttrs.addFlashAttribute("user", userInput);
+		return RedirectConstants.REDIRECT_USUARIO + idUsuario;
+	}
+
+	@PostMapping("/{id}/resetSenha")
+	public String resetarSenhaUsuario(@PathVariable("id") Long idUsuario){
+		return RedirectConstants.REDIRECT_USUARIO + idUsuario;
+	}
+
+	@RequestMapping("/{id}/delete")
+	public String deletarUsuario(@PathVariable("id") Long idUsuario, RedirectAttributes redirectAttrs) {
+		User usuario = userRepository.findById(idUsuario).get();
+		if (usuario == null) {
+			redirectAttrs.addFlashAttribute(StringConstants.ERROR, "Não existe uma usuário com essa identificação.");
+		} else {
+			userRepository.delete(usuario);
+			redirectAttrs.addFlashAttribute(StringConstants.SUCCESS, "Usuário deletado com sucesso.");
+		}
+
+		return RedirectConstants.REDIRECT_USUARIO;
 	}
 }
